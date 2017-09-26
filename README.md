0734a76f7e13eb90017a9ef990388d03e2e5fcf2
Muzammil Khaja Mohammed <muzammilkm@gmail.com>
2017-09-26 15:28:22 +0530

    reodering the code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# js-router
+js-router
+==========
+Built on jQuery 1.3.0 & Inspried by (Angular ui-router & jQuery Single Page Application micro framework).
+
+# Features
+Tiny
+Single Page Application (SPA)
+Seperation Concerns
+Nested Views
+
+# Introduction
+A tiny jQuery plugin for building single page application (SPA) with the possiblily of nested views.
+
--- sample/scripts/router.js
+++ sample/scripts/router.js
@@ -1,114 +1,163 @@
-(function(window, rp) {
-    var router = rp.router = {},
-        env = rp.ENV,
-        templateCache = {},
+(function($, window) {
+    var router,
         isFirstTime = true,
         defaultRoute,
-        viewSelector,
-        current;
+        events = {
+            routeChangeStart: 'router.routeChangeStart',
+            routeChangeSucess: 'router.routeChangeSucess'
+        },
+        render;
 
-    function getViewTemplate(url) {
-        return $.get(url + '?_=' + env.appVer, "html")
-            .then(function(content) {
-                templateCache[url] = content;
-            });
-    }
+    router = (function() {
+        var s = {},
+            current;
 
-    function onhashchange() {
-        var hash = window.location.hash,
-            routes = router.routes,
-            matchedRoute = router.match(hash);
+        function onhashchange() {
+            var hash = window.location.hash,
+                matchedRoute = router.match(hash);
 
-        if (matchedRoute) {
-            var requests = [];
-            for (var i in matchedRoute.segments) {
-                var segment = matchedRoute.segments[i],
-                    route = routes[segment];
+            if (matchedRoute) {
+                $(window).trigger(events.routeChangeStart, [matchedRoute]);
+                render.processRoute(matchedRoute, s.routes)
+                    .then(function() {
+                        render.render(current, matchedRoute, s.routes);
+                        $(window).trigger(events.routeChangeSucess, [matchedRoute]);
+                        current = matchedRoute;
+                    });
+            }
+        }
 
-                if (!templateCache[route.templateUrl]) {
-                    requests.push(getViewTemplate(route.templateUrl));
+        s.match = function(url) {
+            var s = this,
+                route;
+            for (var routeName in s.routes) {
+                if (!s.routes[routeName].abstract && s.routes[routeName].url === url) {
+                    route = s.routes[routeName];
+                }
+            }
+            return route;
+        };
+
+        s.setController = function(ctrl) {
+            render.setController(ctrl);
+            return this;
+        };
+
+        s.setData = function(data) {
+            var s = this;
+
+            s.routes = {};
+            for (var routeName in data) {
+                var segments = routeName.split('.'),
+                    _routeName = [],
+                    url = '',
+                    route = s.routes[routeName] = {
+                        segments: []
+                    };
+
+                for (var i in segments) {
+                    _routeName.push(segments[i]);
+                    var segment = _routeName.join('.');
+
+                    url += data[segment].url;
+                    route.segments.push(segment);
+                }
+                route.url = url;
+                route.templateUrl = data[routeName].templateUrl;
+                route.controller = data[routeName].controller;
+            }
+            return s;
+        };
+
+        s.setDefault = function(name) {
+            defaultRoute = name;
+            return this;
+        };
+
+        s.getCurrentRoute = function() {
+            return render.getCurrentRoute();
+        };
+
+        s.go = function(routeName, params) {
+            var s = this;
+            routeName = routeName || defaultRoute;
+            window.location = s.routes[routeName].url;
+            return s;
+        };
+
+        s.run = function(viewSelector, routeName, params) {
+            var s = this;
+            if (isFirstTime) {
+                render.setViewSelector(viewSelector);
+                $(window).on("hashchange", onhashchange);
+                var route = s.match(window.location.hash);
+                if (!route) {
+                    s.go(routeName, params);
+                } else {
+                    onhashchange();
+                }
+                isFirstTime = false;
+            }
+        };
+
+        return s;
+    }());
+
+
+    render = (function() {
+        var s = {},
+            templateCache = {},
+            controller,
+            viewSelector;
+
+        s.setController = function(ctrl) {
+            controller = ctrl;
+        };
+
+        s.setViewSelector = function(selector) {
+            viewSelector = selector;
+            return this;
+        };
+
+        s.getViewTemplate = function(url) {
+            return $.get(url, "html")
+                .then(function(content) {
+                    templateCache[url] = content;
+                });
+        };
+
+        s.processRoute = function(route, routes) {
+            var s = this,
+                requests = [];
+
+            for (var i in route.segments) {
+                var segment = route.segments[i],
+                    _route = routes[segment];
+
+                if (!templateCache[_route.templateUrl]) {
+                    requests.push(s.getViewTemplate(_route.templateUrl));
                 }
             }
 
-            $.when.apply($, requests)
-                .then(function() {
-                    for (var i in matchedRoute.segments) {
-                        var segment = matchedRoute.segments[i],
-                            route = routes[segment],
-                            $page = $(viewSelector + ':eq(' + i + ')');
-                        if (!current || matchedRoute.segments[i] !== current.segments[i]) {
-                            $page.html(templateCache[route.templateUrl]);
-                            if (rp.controller.is(route.controller)) {
-                                rp.controller.get(route.controller).init();
-                            }
-                        }
+            return $.when.apply($, requests);
+        };
+
+        s.render = function(current, route, routes) {
+            for (var i in route.segments) {
+                var segment = route.segments[i],
+                    _route = routes[segment],
+                    $page = $(viewSelector + ':eq(' + i + ')');
+                if (!current || route.segments[i] !== current.segments[i]) {
+                    $page.html(templateCache[_route.templateUrl]);
+                    if (controller.is(_route.controller)) {
+                        controller.get(_route.controller).init();
                     }
-
-                    current = matchedRoute;
-                });
-        }
-    }
-
-    router.match = function(url) {
-        var s = this,
-            route;
-        for (var routeName in s.routes) {
-            if (!s.routes[routeName].abstract && s.routes[routeName].url === url) {
-                route = s.routes[routeName];
+                }
             }
-        }
-        return route;
-    };
+        };
 
-    router.setData = function(data) {
-        var s = this;
+        return s;
+    }());
 
-        s.routes = {};
-        for (var routeName in data) {
-            var segments = routeName.split('.'),
-                _routeName = [],
-                url = '',
-                route = s.routes[routeName] = {
-                    segments: []
-                };
-
-            for (var i in segments) {
-                _routeName.push(segments[i]);
-                var segment = _routeName.join('.');
-
-                url += data[segment].url;
-                route.segments.push(segment);
-            }
-            route.url = url;
-            route.templateUrl = data[routeName].templateUrl;
-            route.controller = data[routeName].controller;
-        }
-        return this;
-    };
-
-    router.setDefault = function(name) {
-        defaultRoute = name;
-        return this;
-    };
-
-    router.go = function(routeName, params) {
-        routeName = routeName || defaultRoute;
-        window.location = router.routes[routeName].url;
-    };
-
-    router.run = function(vSelector, routeName, params) {
-        if (isFirstTime) {
-            viewSelector = vSelector;
-            $(window).on("hashchange", onhashchange);
-            var route = this.match(window.location.hash);
-            if (!route) {
-                this.go(routeName, params);
-            } else {
-                onhashchange();
-            }
-            isFirstTime = false;
-        }
-    };
-
-
-}(this, RealPage));
+    $.router = router;
+}(jQuery, this));
