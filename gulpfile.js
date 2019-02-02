fa80b266bca03bf0bc4be9e257dba089d0c149f6
Muzammil Khaja Mohammed <muzammilkm@gmail.com>
2019-02-02 16:26:08 +0530

    updating packages & updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,33 @@
 // including plugins
-var gulp = require('gulp'),
-    uglify = require("gulp-uglify"),
-    jshint = require("gulp-jshint"),
-    rename = require("gulp-rename"),
-    concat = require("gulp-concat");
+var gulp = require("gulp"),
+  uglify = require("gulp-uglify"),
+  jshint = require("gulp-jshint"),
+  rename = require("gulp-rename"),
+  concat = require("gulp-concat");
 
-gulp.task('default', function() {
-    gulp
-        .start('jsLint')
-        .start('minify-js');
+// task
+gulp.task("jsLint", function() {
+  return gulp
+    .src("./src/*.js") // path to your files
+    .pipe(jshint())
+    .pipe(jshint.reporter()); // Dump results
 });
 
 // task
-gulp.task('jsLint', function() {
-    gulp.src('./src/*.js') // path to your files
-        .pipe(jshint())
-        .pipe(jshint.reporter()); // Dump results
+gulp.task("minify-js", function() {
+  return gulp
+    .src(["./src/**/*.js"])
+    .pipe(concat("jq-router.js"))
+    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest("docs/scripts"))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest("dist"));
 });
 
-// task
-gulp.task('minify-js', function() {
-    gulp.src(['./src/**/*.js'])
-        .pipe(concat('jq-router.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest('docs/scripts'))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist'));
-});
+
+gulp.task("default", gulp.series("jsLint", "minify-js"));
